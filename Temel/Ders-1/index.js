906a474b86651725b866c5e5ebef3e944d288bcb
Talha YÜKSEK <yuksek_talha@hotmail.com>
2020-12-05 19:26:57 +0300

    ilk ders

---
+++ Temel/Ders-1/index.js
@@ -0,0 +1,73 @@
+document.writeln("Hello Word,")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var metin="Merhaba,Merhaba,Merhaba,Merhaba,Merhaba,Merhaba,Merhaba,Merhaba,Merhaba,Merhaba,Merhaba,";
+//document.write(metin.length);
+
+
+
+
+
+
+
+//document.getElementById("h1").innerHTML = "Merhaba";
+//document.getElementById("h1").innerHTML = "Merhaba";
+
+
+/*var mesaj="merhaba dunya ne haber";
+alert("Merhaba Javascript "+mesaj);
+console.log(mesaj);
+window.alert(mesaj);
+document.write("merhabaa");
+document.getElementsByTagName("h1").innerHTML = "asdasf";
+debugger;
+
+let isim="Talha\n";
+if(true)
+{
+    let isim="\nYuksek"
+    document.write(isim);
+}
+document.write(isim);
+*/
